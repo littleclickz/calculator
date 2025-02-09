5af8c6c7d9a4a7f6b13ee33eae66d77caa6cb888
littleclickz <hnvanpatten@gmail.com>
2025-02-08 18:06:56 -0800

    Create the functions for the basic math operators

---
+++ script.js
@@ -0,0 +1,35 @@
+let num1 = 4; //change after testing
+
+let operator = "*"; //change after testing
+
+let num2 = 2; //change after testing
+
+function add() {
+  let additionTotal = num1 + num2;
+  return additionTotal;
+}
+
+function subtract() {
+  let subtractionTotal = num1 - num2;
+  return subtractionTotal;
+}
+
+function multiply() {
+  let multiplicationTotal = num1 * num2;
+  return multiplicationTotal;
+}
+
+function divide() {
+  let divisionTotal = num1 / num2;
+  return divisionTotal;
+}
+
+console.log(add()); //should show 6
+
+console.log(subtract()); //should show 2
+
+console.log(multiply()); //should show 8
+
+console.log(divide()); //should show 2
+
+//to do: test this code (check the console log)
