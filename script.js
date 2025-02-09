2784480421dec3a9de6e9760438f68cc2ad0f76d
littleclickz <hnvanpatten@gmail.com>
2025-02-08 18:44:13 -0800

    Add event listeners for all the digit buttons and make event listeners for the operator buttons update the display field too

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let operator;
 
 let string2 = "";
 
+const displayField = document.getElementById("#display-field");
+
 function convert() {
   let num1 = Number(string1);
   let num2 = Number(string2);
@@ -45,31 +47,139 @@ function operate() {
   }
 }
 
-console.log(operator); //should show "null"; otherwise change the "operator != null" thing below accordingly
-
 const additionButton = document.querySelectory("#addition-button");
 additionButton.addEventListener("click", () => {
   let operator = "+";
+  displayField.textContent = operator;
 });
 
 const subtractionButton = document.querySelectory("#subtraction-button");
 subtractionButton.addEventListener("click", () => {
   let operator = "-";
+  displayField.textContent = operator;
 });
 
 const multiplicationButton = document.querySelectory("#multiplication-button");
 multiplicationButton.addEventListener("click", () => {
   let operator = "*";
+  displayField.textContent = operator;
 });
 
 const divisionButton = document.querySelectory("#division-button");
 divisionButton.addEventListener("click", () => {
   let operator = "/";
+  displayField.textContent = operator;
 });
 
-//what to put inside event listener for digit buttons:
-  //if (operator === null) { //experiment with "undefined" if "null" doesn't work
-    //
-  //}
+const digit1 = document.querySelector("#digit1");
+digit1.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "1";
+    displayField.textContent = string1;
+  } else {
+    string2 += "1";
+    displayField.textContent = string2;
+  }
+});
 
-//to do: test (check console log, then click an operator button and check log again)
+const digit2 = document.querySelector("#digit2");
+digit2.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "2";
+    displayField.textContent = string1;
+  } else {
+    string2 += "2";
+    displayField.textContent = string2;
+  }
+});
+
+const digit3 = document.querySelector("#digit3");
+digit3.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "3";
+    displayField.textContent = string1;
+  } else {
+    string2 += "3";
+    displayField.textContent = string2;
+  }
+});
+
+const digit4 = document.querySelector("#digit4");
+digit4.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "4";
+    displayField.textContent = string1;
+  } else {
+    string2 += "4";
+    displayField.textContent = string2;
+  }
+});
+
+const digit5 = document.querySelector("#digit5");
+digit5.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "5";
+    displayField.textContent = string1;
+  } else {
+    string2 += "5";
+    displayField.textContent = string2;
+  }
+});
+
+const digit6 = document.querySelector("#digit6");
+digit6.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "6";
+    displayField.textContent = string1;
+  } else {
+    string2 += "6";
+    displayField.textContent = string2;
+  }
+});
+
+const digit7 = document.querySelector("#digit7");
+digit7.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "7";
+    displayField.textContent = string1;
+  } else {
+    string2 += "7";
+    displayField.textContent = string2;
+  }
+});
+
+const digit8 = document.querySelector("#digit8");
+digit8.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "8";
+    displayField.textContent = string1;
+  } else {
+    string2 += "8";
+    displayField.textContent = string2;
+  }
+});
+
+const digit9 = document.querySelector("#digit9");
+digit9.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "9";
+    displayField.textContent = string1;
+  } else {
+    string2 += "9";
+    displayField.textContent = string2;
+  }
+});
+
+const digit0 = document.querySelector("#digit0");
+digit0.addEventListener("click", () => {
+  if (operator === null) { //experiment with "undefined" if "null" doesn't work
+    string1 += "0";
+    displayField.textContent = string1;
+  } else {
+    string2 += "0";
+    displayField.textContent = string2;
+  }
+});
+
+
+//to do: test (click a digit button and see if the digit appears in the display field; click an operator button and see if the operator appears in the display field)
